04a6594723c60be9cd7446958914a26be781c662
William Best <wmbest2@gmail.com>
2012-05-17 21:15:33 -0500

    Initial working commit, effectively computes

---
+++ Ingredient.py
@@ -0,0 +1,68 @@
+import math
+
+class ImageChunk:
+    pass
+
+class Material:
+    def __init__(self, name):
+        self.name = name
+        self.makes = 1.0
+
+    def output_for_count(self, count = 1):
+        return str(float(count)) + " " + self.name + "\n"
+
+    def output_totals(self, totals, count):
+        if totals.has_key(self.name) != True:
+            totals[self.name] = float(0)
+        totals[self.name] += float(count)
+
+class Recipe(Material):
+    def __init__(self, name, makes=1.0):
+        self.name = name
+        self.ingredients=[]
+        self.makes=float(makes)
+
+    def output_for_count(self, count = 1.0):
+        output = Material.output_for_count(self, count);
+        for i in self.ingredients:
+            output += i.material.output_for_count(i.count)
+        return output
+
+    def output_totals(self, totals, count = 1.0):
+        for i in self.ingredients:
+            i.material.output_totals(totals, float(count) * (float(i.count) / i.material.makes))
+
+    def print_totals(self):
+        totals = dict()
+        self.output_totals(totals, 1.0)
+
+        print Material.output_for_count(self);
+        print "Totals -----------------------------------------"
+        for k in totals.keys():
+            print k + " " + str(math.ceil(totals[k]))
+
+    @staticmethod
+    def load_from_json(json):
+        recipes = dict()
+        for i in json:
+            if i['type'] == 0:
+                recipes[i['name']] = Material(i['name'])
+            else:
+                if i.has_key('makes'):
+                    recipe = Recipe(i['name'], i['makes'])
+                else:
+                    recipe = Recipe(i['name'])
+                for ing in i['ingredients']:
+                    if ing.has_key('count'):
+                        recipe.ingredients.append(Ingredient(recipes[ing['name']], ing['count']))
+                    else:
+                        recipe.ingredients.append(Ingredient(recipes[ing['name']]))
+                recipes[i['name']] = recipe
+        return recipes
+
+
+class Ingredient:
+    def __init__(self, material, count = 1.0):
+        self.material = material
+        self.count = float(count)
+
---
+++ test.py
@@ -0,0 +1,14 @@
+from Ingredient import *
+import json
+from pprint import pprint
+
+item_data = open('recipes.json')
+items = json.load(item_data)['recipes']
+item_data.close()
+
+recipes = Recipe.load_from_json(items);
+
+my_recipe = Recipe("Cool stuff ahead")
+my_recipe.ingredients.append(Ingredient(recipes['Redstone Repeater'], 1))
+my_recipe.print_totals()
+
