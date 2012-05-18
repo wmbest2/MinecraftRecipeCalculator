ccd29ba98494f1c16290410ba4a1420154d1bed6
William Best <wmbest2@gmail.com>
2012-05-18 06:48:15 -0500

    Added more examples and recipes

--- test.py
+++ test.py
@@ -11,6 +11,26 @@ recipes = Recipe.load_from_json(items);
 my_recipe = Recipe("Cool stuff ahead")
 my_recipe.ingredients.append(Ingredient(recipes['Redstone Repeater'], 1))
 my_recipe.print_ingredients()
-print ""
+print
+my_recipe.print_totals()
+
+print
+print
+
+my_recipe = Recipe("Kaboom")
+my_recipe.ingredients.append(Ingredient(recipes['TNT Block'], 4))
+my_recipe.ingredients.append(Ingredient(recipes['Redstone Repeater'], 1))
+my_recipe.print_ingredients()
+print
+my_recipe.print_totals()
+
+print
+print
+
+my_recipe = Recipe("Hidden Door")
+my_recipe.ingredients.append(Ingredient(recipes['Sticky Piston'], 6))
+my_recipe.ingredients.append(Ingredient(recipes['Redstone Repeater'], 8))
+my_recipe.print_ingredients()
+print
 my_recipe.print_totals()
 
